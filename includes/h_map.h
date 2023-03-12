c5fa954e3ec1f2538137fe1b46e664a4e9a09a3c
amongloneliness <vugi5000@gmail.com>
2023-03-12 05:53:59 +0500

    creating project files

---
+++ includes/h_map.h
@@ -0,0 +1,7 @@
+#ifndef H_MAP_H
+#define H_MAP_H
+
+#include <string>
+#include "../sfml/h_sfml.h"
+
+#endif
---
+++ sfml/h_sfml.h
@@ -0,0 +1,9 @@
+#ifndef H_SFML_H
+#define H_SFML_H
+
+#include <SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
+
+using namespace sf;
+
+#endif
